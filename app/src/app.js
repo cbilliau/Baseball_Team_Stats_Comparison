8b4afa177bfea74bce69231adc15e525dbd817e6
cbilliau <christopher@billiau.com>
2016-09-10 16:14:25 -0400

    Created a factory and reconfigured  call. Now able to acquire data from from factory in directive.

--- app/src/app.js
+++ app/src/app.js
@@ -2,41 +2,40 @@
 
 angular.module('baseballStatsApp', ['ngMaterial', 'ngMdIcons'])
 
-    .config(function($mdIconProvider, $mdThemingProvider, $httpProvider) {
-        // $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-        // define the icon names for shortcuts and icon locations
-        // $mdIconProvider
-        //     .defaultIconSet()
-        //     .icon();
-        // define theme pallete
-        $mdThemingProvider.theme('default')
-            .primaryPalette('blue')
-            .accentPalette('red');
-      })
+.config(function($mdIconProvider, $mdThemingProvider, $httpProvider) {
+    // $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    // define the icon names for shortcuts and icon locations
+    // $mdIconProvider
+    //     .defaultIconSet()
+    //     .icon();
+    // define theme pallete
+    $mdThemingProvider.theme('default')
+        .primaryPalette('blue')
+        .accentPalette('red');
+})
 
-      .service('appService', function($http, $q) {
-          let service = this;
-          let url = 'https://erikberg.com/mlb/standings.json';
-          service.getStats = function() {
-              $http.get(url)
-                  .then(function(response) {
-                      return $q.when(response);
-                  }),
-                  function(response) {
-                    alert('error');
-                  };
-          }
-      })
 
-      .directive('mainContent', function (appService) {
-        return {
-          restrict: 'A',
-          templateUrl: './src/views/teamComparison-template.html',
-          controller: function controller($scope, $element, $attrs) {
-            this.appService = appService;
-            this.data = this.appService.getStats()
-            console.log(this.data);
+.factory('standings', function($http) {
+    return function() {
+        return $http({
+            url: 'https://erikberg.com/mlb/standings.json'
+        });
+    }
+})
+
+
+.directive('mainContent', function(standings) {
+    return {
+        restrict: 'A',
+        templateUrl: './src/views/teamComparison-template.html',
+        controller: function controller($scope, $element, $attrs) {
+
+            // Call factory
+            standings().success(function(results) {
+                $scope.standingsData = results;
+                console.log($scope.standingsData);
+            });
         }
-      }
-    });
+    }
+});
