cc181ad4e7fedaf7653db54f515ad1e179d7d088
cbilliau <christopher@billiau.com>
2016-09-10 06:56:39 -0400

    Starting over with angular setup due to loss of data when trying to revert to previous commit.

--- app/src/app.js
+++ app/src/app.js
@@ -1,7 +1,11 @@
-'use strict'
-
-var BaseballStatsApp;
-
-(function (BaseballStatsApp) {
-  angular.module('baseballStatsApp', ['ngMaterial'])
-})(BaseballStatsApp || (BaseballStatsApp = {}));
+angular.module('baseballStatsApp', ['ngMaterial', 'ngMdIcons'])
+    .config(function ($mdIconProvider, $mdThemingProvider) {
+    // define the icon names for shortcuts and icon locations
+    // $mdIconProvider
+    //     .defaultIconSet()
+    //     .icon();
+    // define theme pallete
+    $mdThemingProvider.theme('default')
+        .primaryPalette('blue')
+        .accentPalette('red');
+});
---
+++ app/src/controllers/mainController.js
@@ -0,0 +1,4 @@
+angular.module('mainController', ['$mdSidenav'])
+    .controller(function () {
+
+});
