a23c3dc4a2bf46282db38b96f74ee3278935a822
cbilliau <christopher@billiau.com>
2016-09-10 13:24:24 -0400

    Figured ut custom directive issue. In Angular Material, a custom directive cannot be an element. I switched the directive to an attribute and placed the custom directive attribute on the div where I wanted the template to load and it worked. Now to solve the CORS issue with getting the data from the external API.

--- app/src/app.js
+++ app/src/app.js
@@ -4,7 +4,7 @@ angular.module('baseballStatsApp', ['ngMaterial', 'ngMdIcons'])
 
     .config(function($mdIconProvider, $mdThemingProvider, $httpProvider) {
         $httpProvider.defaults.useXDomain = true;
-        // delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
         // define the icon names for shortcuts and icon locations
         // $mdIconProvider
         //     .defaultIconSet()
@@ -17,9 +17,9 @@ angular.module('baseballStatsApp', ['ngMaterial', 'ngMdIcons'])
 
       .service('appService', function($http, $q) {
           let service = this;
-          service.url = 'https://erikberg.com/mlb/standings.json';
+          let url = 'https://erikberg.com/mlb/standings.json';
           service.getStats = function() {
-              $http.get(service.url)
+              $http.get(url)
                   .then(function(response) {
                       return $q.when(response);
                   }),
@@ -29,9 +29,9 @@ angular.module('baseballStatsApp', ['ngMaterial', 'ngMdIcons'])
           }
       })
 
-      .directive('mainContent', function (appService) {
+      .directive('mainContent', function (appService, $mdSidenav, $mdToast, $mdDialog, $mdMedia, $mdBottomSheet) {
         return {
-          restrict: 'E',
+          restrict: 'A',
           templateUrl: './src/views/teamComparison-template.html',
           controller: function controller($scope, $element, $attrs) {
             console.log($attrs);
