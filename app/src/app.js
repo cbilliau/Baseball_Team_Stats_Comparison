999dce08c8c07c3c7c92939e225de5e219c3ab9b
cbilliau <christopher@billiau.com>
2016-09-11 16:09:36 -0400

    Completed logic factory that seperates data into two objects representing teams for al and nl. Unfortunately, the API only provides actual stats and not team standing data.

--- app/src/app.js
+++ app/src/app.js
@@ -23,6 +23,7 @@ angular.module('baseballStatsApp', ['ngMaterial', 'ngMdIcons'])
         return $http({
             method: 'GET',
             url: 'https://api.stattleship.com/baseball/mlb/team_season_stats',
+            // params: {},
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Token token=031488b51ba97cd59d3934f56f21c355',
@@ -32,14 +33,78 @@ angular.module('baseballStatsApp', ['ngMaterial', 'ngMdIcons'])
     }
 })
 
-.factory('generateStandings'function() {
-    return function(data) {
-      
+.factory('generateStandings', function() {
+
+    // Define objects that will hold teams
+    var alStandings = {
+        teams: []
+    };
+    var nlStandings = {
+        teams: []
+    };
+
+    return {
+
+        // Push data into standings
+        pushToObject: function(obj, team, team_stats, conf) {
+
+            obj.teams.push({
+                name: team.name,
+                nickName: team.nickname,
+                colors: team.colors,
+                wins: team_stats.wins,
+                losses: team_stats.losses,
+                balls: team_stats.balls,
+                hits: team_stats.hits,
+                runs: team_stats.runs,
+                singles: team_stats.singles,
+                doubles: team_stats.doubles,
+                triples: team_stats.triples,
+                rbis: team_stats.rbi,
+                era: team_stats.era,
+                runs_allowed: team_stats.runs_allowed,
+                double_plays: team_stats.double_plays,
+                triple_plays: team_stats.triple_plays,
+                division_id: team.division_id
+            })
+        },
+
+        // Generate national league and american league standing objects
+        generate: function(data) {
+
+            var alConfernceId = data.divisions[0].conference_id;
+            var nlConferenceId = data.divisions[1].conference_id;
+            var alEastDivisionId = data.divisions[0].id;
+            var nlEastDivisionId = data.divisions[1].id;
+            var nlWestDivisionId = data.divisions[2].id;
+            var alWestDivisionId = data.divisions[3].id;
+            var nlCentralDivisionId = data.divisions[4].id;
+            var alCentralDivisionId = data.divisions[5].id;
+
+            // iterate through data to sort by teams and then send to push func to be pushed into conference array
+            for (var i = 0; i < data.teams.length; i++) {
+                if (
+                    data.teams[i].id === data.team_season_stats[i].team_id &&
+                    data.teams[i].division_id === alEastDivisionId ||
+                    data.teams[i].division_id === alWestDivisionId ||
+                    data.teams[i].division_id === alCentralDivisionId
+                ) {
+                    var conference = 'American League';
+                    this.pushToObject(alStandings, data.teams[i], data.team_season_stats[i], conference);
+
+                } else {
+                    var conference = 'National League';
+                    this.pushToObject(nlStandings, data.teams[i], data.team_season_stats[i], conference);
+                }
+            }
+
+            // Return objects containing teams and their stats 
+            return [alStandings, nlStandings];
+        }
     }
 })
 
-
-.directive('mainContent', function(callAPI) {
+.directive('mainContent', function(callAPI, generateStandings) {
     return {
         restrict: 'A',
         templateUrl: './src/views/mainContent-template.html',
@@ -48,15 +113,9 @@ angular.module('baseballStatsApp', ['ngMaterial', 'ngMdIcons'])
 
             // Call API factory
             callAPI().success(function(results) {
-                console.log(results.conferences[0].league_id);
-                console.log(results.conferences[0].name);
-                console.log(results.divisions[0].conference_id);
-                console.log(results.divisions[0].name);
-                console.log(results.teams[0].id);
-                console.log(results.teams[0].name);
-
-
-                // $scope.callAPIData = results;
+                console.log(results);
+                $scope.standings = generateStandings.generate(results);
+                console.log($scope.standings);
             });
         }
     }
