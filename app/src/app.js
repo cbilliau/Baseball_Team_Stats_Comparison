8ce7457e840a190804f23de01c5a992e2914ad4b
cbilliau <christopher@billiau.com>
2016-09-12 11:07:02 -0400

    Added toggleSideNav function to mainContent directive controller and svg menu icons to assets dir.

--- app/src/app.js
+++ app/src/app.js
@@ -6,9 +6,9 @@ angular.module('baseballStatsApp', ['ngMaterial', 'ngMdIcons'])
     // $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
     // define the icon names for shortcuts and icon locations
-    // $mdIconProvider
-    //     .defaultIconSet()
-    //     .icon();
+    $mdIconProvider
+        .defaultIconSet('./assets/svg/avatars.svg', 128)
+        .icon('menu', './assets/svg/menu.svg', 24);
     // define theme pallete
     $mdThemingProvider.theme('default')
         .primaryPalette('blue')
@@ -16,7 +16,9 @@ angular.module('baseballStatsApp', ['ngMaterial', 'ngMdIcons'])
 })
 
 // Placeholder for controller if needed
-.controller('mainController', function(mainContent, sideBar) {})
+.controller('mainController', function(mainContent, sideBar) {
+
+})
 
 .factory('callAPI', function($http) {
     return function() {
@@ -108,15 +110,18 @@ angular.module('baseballStatsApp', ['ngMaterial', 'ngMdIcons'])
     return {
         restrict: 'A',
         templateUrl: './src/views/mainContent-template.html',
-        controller: function controller($scope, $element, $attrs) {
+        controller: function controller($scope, $element, $attrs, $mdSidenav) {
+            this.$mdSidenav = $mdSidenav;
 
-
-            // Call API factory
             callAPI().success(function(results) {
                 console.log(results);
                 $scope.standings = generateStats.iterateLeagueData(results);
                 console.log($scope.standings);
             });
+
+            $scope.toggleSideNav = function () {
+                $mdSidenav('left').toggle();
+            };
         }
     }
 })
@@ -128,69 +133,6 @@ angular.module('baseballStatsApp', ['ngMaterial', 'ngMdIcons'])
         controller: function controller($scope, $element, $attrs, $timeout) {
 
 
-
-
-            // In this example, we set up our model using a class.
-            // Using a plain object works too. All that matters
-            // is that we implement getItemAtIndex and getLength.
-            var DynamicItems = function() {
-                /**
-                 * @type {!Object<?Array>} Data pages, keyed by page number (0-index).
-                 */
-                this.loadedPages = {};
-
-                /** @type {number} Total number of items. */
-                this.numItems = 0;
-
-                /** @const {number} Number of items to fetch per request. */
-                this.PAGE_SIZE = 50;
-
-                this.fetchNumItems_();
-            };
-
-            // Required.
-            DynamicItems.prototype.getItemAtIndex = function(index) {
-                var pageNumber = Math.floor(index / this.PAGE_SIZE);
-                var page = this.loadedPages[pageNumber];
-
-                if (page) {
-                    return page[index % this.PAGE_SIZE];
-                } else if (page !== null) {
-                    this.fetchPage_(pageNumber);
-                }
-            };
-
-            // Required.
-            DynamicItems.prototype.getLength = function() {
-                return this.numItems;
-            };
-
-            DynamicItems.prototype.fetchPage_ = function(pageNumber) {
-                // Set the page to null so we know it is already being fetched.
-                this.loadedPages[pageNumber] = null;
-
-                // For demo purposes, we simulate loading more items with a timed
-                // promise. In real code, this function would likely contain an
-                // $http request.
-                $timeout(angular.noop, 300).then(angular.bind(this, function() {
-                    this.loadedPages[pageNumber] = [];
-                    var pageOffset = pageNumber * this.PAGE_SIZE;
-                    for (var i = pageOffset; i < pageOffset + this.PAGE_SIZE; i++) {
-                        this.loadedPages[pageNumber].push(i);
-                    }
-                }));
-            };
-
-            DynamicItems.prototype.fetchNumItems_ = function() {
-                // For demo purposes, we simulate loading the item count with a timed
-                // promise. In real code, this function would likely contain an
-                // $http request.
-                $timeout(angular.noop, 300).then(angular.bind(this, function() {
-                    this.numItems = 50000;
-                }));
-            };
-
-            this.dynamicItems = new DynamicItems();
         }
     }
 });
